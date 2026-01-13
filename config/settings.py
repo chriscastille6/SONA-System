70ced59cdc33f52b0f8e13c8c3cab0ff48484360
Christopher Castille <christopher.castille@nicholls.edu>
2026-01-13 13:18:13 -0600

    Change site name from HSIRB to Participant Recruitment and Management System (PRAMS)

--- config/settings.py
+++ config/settings.py
@@ -186,7 +186,7 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = config('CORS_ALLOWED_ORIGINS', default='http://localhost:3000', cast=Csv())
 
 # Site Configuration
-SITE_NAME = config('SITE_NAME', default='Research Participant System')
+SITE_NAME = config('SITE_NAME', default='Participant Recruitment and Management System (PRAMS)')
 SITE_URL = config('SITE_URL', default='http://localhost:8000')
 INSTITUTION_NAME = config('INSTITUTION_NAME', default='Nicholls State University')
 
